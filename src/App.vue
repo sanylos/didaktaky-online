b23c8910032106d1c81d3175e08fb9f1c353c834
sanyl <homolkadam@seznam.cz>
2023-12-06 20:46:28 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">
+    <h1>HomePage</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
