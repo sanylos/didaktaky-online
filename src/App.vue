1098ba5f111acc89c778fb21a916952579608c02
sanyl <homolkadam@seznam.cz>
2023-12-07 19:54:52 +0100

    Navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
+    <Navbar/>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
     <RouterView />
