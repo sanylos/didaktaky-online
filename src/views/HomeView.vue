cac76f5bce40c5c790dcfb580dc2c7d8bf801940
sanyl <homolkadam@seznam.cz>
2023-12-08 20:43:23 +0100

    Home question container added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="">
-    <h1>HomePage</h1>
+  <div class="main-page">
+    <div class="question-container bg-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+      repellendus minima quis tempore, iste pariatur neque, aut consectetur aliquam deleniti accusamus maxime atque rem
+      voluptatem id explicabo, omnis non fugit!</div>
+  
+  <div class="footer">
+    <span>Prosím podpořte tento projekt <a href="https://www.buymeacoffee.com/sanyl">zde</a></span>
   </div>
+</div>
 </template>
 
-<style>
+<style scoped>
+.main-page {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
 
+.question-container {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+  width: 50rem;
+  height: 35rem;
+  max-width: 90vw;
+  max-height: 80vh;
+}
+.footer {
+  position: absolute;
+  bottom: 1rem;
+}
 </style>
