0867c54a7f916b2a54b1f9bc886b5db9016cfd8b
sanyl <homolkadam@seznam.cz>
2023-12-09 22:18:11 +0100

    Navbar path changed

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import Navbar from './components/Navbar.vue';
+import Navbar from '@/components/Navbar.vue';
 </script>
 
 <style scoped>
