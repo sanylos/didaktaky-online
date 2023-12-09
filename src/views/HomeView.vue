fe8942b1628e6fcea6dcba01c3d6ca243edd42bd
sanyl <homolkadam@seznam.cz>
2023-12-09 22:18:26 +0100

    Added question fetching

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,34 +5,46 @@
     </div>
     <div class="container-fluid bg-dark w-auto rounded p-3 shadow-lg m-1">
       <div class="question-title">
-        5 Které z následujících tvrzení o slovesném tvaru podtrženém v TEXTU 1 je
-pravdivé?
+        <div>
+          <b>{{ exercises.number }}</b>
+          {{ exercises.title }}
+        </div>
       </div>
       <br>
-      <div class="question-option">
-        A) Tvar ztráceje v textu vyjadřuje děj následující po jiném ději – dotyčný prchal
-a chvíli poté začal ztrácet rovnováhu.
-      </div>
-      <div class="question-option">
-        A) Tvar ztráceje v textu vyjadřuje děj následující po jiném ději – dotyčný prchal
-a chvíli poté začal ztrácet rovnováhu.
-      </div>
-      <div class="question-option">
-        A) Tvar ztráceje v textu vyjadřuje děj následující po jiném ději – dotyčný prchal
-a chvíli poté začal ztrácet rovnováhu.
-      </div>
-      <div class="question-option">
-        A) Tvar ztráceje v textu vyjadřuje děj následující po jiném ději – dotyčný prchal
-a chvíli poté začal ztrácet rovnováhu.
+      <div>
+        <div v-for="option, index in exercises.answers" class="question-option">
+          <div :class="[(option==exercises.correct_answer)&(answer == option) ? 'bg-success' : ''],
+          [(option!=exercises.correct_answer)&(answer == option) ? 'bg-danger' : '']" class="rounded p-2 mb-1"
+            @click="answer = option">{{ option }}</div>
+        </div>
+        {{ answer }}
       </div>
     </div>
-  
-  <div class="footer">
-    <a href="https://www.buymeacoffee.com/sanyl">PODPORA PROJEKTU</a>
+    <div class="footer">
+      <a href="https://www.buymeacoffee.com/sanyl">PODPORA PROJEKTU</a>
+    </div>
   </div>
-</div>
 </template>
 
+<script lang="ts" setup>
+import { supabase } from '@/supabase'
+import { reactive, ref } from 'vue';
+
+
+let answer = ref('fsdfd');
+let exercises = ref([]);
+
+const getQuestion = async () => {
+  const { data } = await supabase
+    .from('random_exercise')
+    .select('*')
+    .limit(1);
+  exercises.value = data[0];
+  console.log(exercises.value)
+}
+getQuestion();
+
+</script>
 <style scoped>
 .main-page {
   display: flex;
@@ -40,6 +52,7 @@ a chvíli poté začal ztrácet rovnováhu.
   flex-direction: column;
   justify-content: center;
 }
+
 .page-title {
   font-size: 3em;
 }
