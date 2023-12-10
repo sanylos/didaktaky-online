ecb4d7b196c70a5730a80ff290e1a1d455de4f04
sanyl <homolkadam@seznam.cz>
2023-12-10 20:36:30 +0100

    Option submit added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="main-page">
     <div class="page-title">
-      <p>Náhodné cvičení</p>
+      Náhodné cvičení
     </div>
-    <div class="container-fluid bg-dark w-auto rounded p-3 shadow-lg m-1">
+    <div class="container-md bg-dark rounded p-3 shadow-lg m-1 w-100">
       <div class="question-title">
         <div class="fw-bolder">
           {{ exercises.number }}
@@ -16,14 +16,17 @@
       <div>
         <div v-for="option, index in exercises.answers" :key="index" class="question-option mb-1">
           <input type="radio" class="btn-check" name="options-base" :id="'option' + index" autocomplete="off"
-            :value="option" v-model="answer">
-          <label class="btn text-white fw-normal" :for="'option' + index">{{ option }}</label>
+            :value="index" v-model="answer" :disabled="answered">
+          <label class="btn text-white fw-normal" :for="'option' + index" :class="{
+            'bg-success': index == exercises.correct_answer && answered,
+            'bg-danger': index == answer && index != exercises.correct_answer && answered,
+          }">{{ option }}</label>
         </div>
       </div>
       <div class="mt-4 d-flex justify-content-between">
         <button class="btn btn-primary">⮜ Předchozí</button>
-        <button class="btn btn-outline-success">✍🏼 Zkontrolovat</button>
-        <button class="btn btn-primary">Další ⮞</button>
+        <button v-if="!answered" class="btn btn-success" @click="handleSubmit" :disabled="answer==null">✍🏼 Zkontrolovat</button>
+        <button v-if="answered" class="btn btn-primary" @click="getQuestion">Další ⮞</button>
       </div>
     </div>
     <div class="footer">
@@ -37,14 +40,20 @@ import { supabase } from '@/supabase'
 import { reactive, ref } from 'vue';
 
 
-let answer = ref('');
+let answer = ref(null);
 let exercises: any = ref([]);
 let answered = ref(false);
+
+const handleSubmit = () => {
+  answered.value = true;
+}
 
 const getQuestion = async () => {
   const { data } = await supabase
     .from('random_exercise')
     .select('*');
+  answered.value = false;
+  answer.value=null;
   exercises.value = data[0];
   console.log(exercises.value)
 }
@@ -60,7 +69,7 @@ getQuestion();
 }
 
 .page-title {
-  font-size: 3em;
+  font-size: 2em;
 }
 
 .footer {
