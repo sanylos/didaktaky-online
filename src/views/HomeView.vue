c79dae46c15d57e5db9ba02194e8f5acf8831952
sanyl <homolkadam@seznam.cz>
2023-12-12 20:37:04 +0100

    Exercise texts added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,23 +14,34 @@
 
       <div v-if="!loading">
 
+<div class="d-flex flex-column justify-content-center align-items-center">
+
+      <div v-if="exercises.text1imgPath" class="col mb-1">
+        <span class="row fw-bold">TEXT 1</span>
+        <img class="exercise-text-image" :src="'src/assets/exercise_texts/'+exercises.text1imgPath+'.png'">
+      </div>
+      <div v-if="exercises.text2imgPath" class="col mb-1">
+        <span class="row fw-bold">TEXT 2</span>
+        <img class="exercise-text-image" :src="'src/assets/exercise_texts/'+exercises.text2imgPath+'.png'">
+      </div>
+
         <div class="question-title">
-          <div class="fw-bolder">
-            {{ exercises.number }}
-            {{ exercises.title }}
+          <div class="">
+            <span class="m-1 fw-bold" v-html="exercises.title"></span>
           </div>
         </div>
 
-        <div v-if="exercises.description">
+        <div v-if="exercises.description" class="">
           ({{ exercises.description }})
         </div>
 
+        <!--SINGLE OPTION-->
         <div class="exercise-options" v-if="exercises.type == 'option'">
           <div>
             <div v-for="option, index in exercises.answers" :key="index" class="question-option mb-1">
               <input type="radio" class="btn-check" name="options-base" :id="'option' + index" autocomplete="off"
                 :value="index" v-model="answer" :disabled="answered">
-              <label class="btn text-white fw-normal" :for="'option' + index" :class="{
+              <label class="btn text-white fw-normal bg-unanswered" :for="'option' + index" :class="{
                 'bg-success': index == exercises.correct_answer && answered,
                 'bg-danger': index == answer && index != exercises.correct_answer && answered,
               }">{{ option }}</label>
@@ -38,6 +49,7 @@
           </div>
         </div>
 
+        <!--ANO/NE OPTIONS-->
         <div class="exercise-options" v-if="exercises.type == 'anone'">
           <div>
             <div class="row text-center">
@@ -46,20 +58,21 @@
               <div class="col-1 p-1">ANO</div>
               <div class="col-1 p-1">NE</div>
             </div>
-            <div v-for="option, index in exercises.answers" :key="index"
-              class="row question-option mb-1 mt-1 p-1 rounded" :class="{
+            <div v-for="option, index in exercises.answers" :key="index" class="row question-option mb-1 mt-1 p-1 rounded"
+              :class="{
                 'bg-success': answer[index] == exercises.correct_answer[index] && answered,
                 'bg-danger': answer[index] != exercises.correct_answer[index] && answered,
               }">
               <p class="col-10">{{ option }}</p>
-              <input type="radio" class="col text-white fw-normal" v-model="answer[index]" :name="'input' + index"
+              <input type="radio" class="col text-white fw-normal radio-anone" v-model="answer[index]" :name="'input' + index"
                 :id="'option' + index" autocomplete="off" value="ANO" :disabled="answered">
-              <input type="radio" class="col text-white fw-normal" v-model="answer[index]" :name="'input' + index"
+              <input type="radio" class="col text-white fw-normal radio-anone" v-model="answer[index]" :name="'input' + index"
                 :id="'option' + index" autocomplete="off" value="NE" :disabled="answered">
             </div>
           </div>
         </div>
 
+      </div>
       </div>
 
       <div class="mt-4 d-flex justify-content-between">
@@ -76,12 +89,13 @@
 <script lang="ts" setup>
 import LoginModal from '@/components/LoginModal.vue';
 import { supabase } from '@/supabase'
+import { getTextOfJSDocComment } from 'typescript';
 import { reactive, ref } from 'vue';
 
 let loading = ref(false);
 let isTextShown = ref(false);
 let exercises: any = ref([]);
-let answer = ref([]);
+let answer: any = ref([]);
 let answered = ref(false);
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
@@ -98,10 +112,11 @@ const getQuestion = async () => {
   answered.value = false;
   answer.value = null;
   exercises.value = data[0];
-  if (exercises.value.type == 'anone') answer.value = Array(exercises.value.answers.length).fill(null);
+  if (exercises.value.type == 'anone') answer.value = Array(exercises.value.answers.length).fill(null); //If exercise type is ANO/NE, set array to null
   console.log(exercises.value)
   loading.value = false;
 }
+
 getQuestion();
 
 </script>
@@ -115,4 +130,16 @@ getQuestion();
 
 .page-title {
   font-size: 2em;
-}</style>
+}
+
+.exercise-text-image {
+  max-width: 100%;
+}
+.bg-unanswered {
+  background-color: rgba(245, 245, 220, 0.036);
+}
+.radio-anone {
+  width: 2rem;
+  height: 2rem;
+}
+</style>
