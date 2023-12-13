afe80e631bde2e2bf94513b837bc5c2907ef1987
sanyl <homolkadam@seznam.cz>
2023-12-13 20:59:39 +0100

    userStore added

---
+++ src/stores/user.ts
@@ -0,0 +1,30 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import { supabase } from '@/supabase'
+
+export const useUserStore = defineStore({
+  id: 'user',
+  state: () => ({
+    id: null,
+    email: null,
+    isLoggedIn: false,
+  }),
+
+  actions: {
+    async initialize() {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user) {
+        this.email = user.email;
+        this.isLoggedIn = true;
+        this.id = user.id;
+      } else {
+        this.isLoggedIn = false;
+      }
+    },
+    async signOut() {
+      const { error } = await supabase.auth.signOut();
+      this.isLoggedIn = false;
+      if (error) alert(error);
+    }
+  },
+})
