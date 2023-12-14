3cafaea235a9d873ff546a074ca3c198fb9f5e0c
sanyl <homolkadam@seznam.cz>
2023-12-14 19:49:54 +0100

    Navbar updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">PRJMCKY</a>
+      <a class="navbar-brand" href="#">DIDAKTAKY-ONLINE</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -28,7 +28,6 @@
 import { useUserStore } from '@/stores/user';
 import { storeToRefs } from 'pinia';
 const userStore = useUserStore();
-const { isLoggedIn } = storeToRefs(userStore);
 </script>
 
 <style scoped>
