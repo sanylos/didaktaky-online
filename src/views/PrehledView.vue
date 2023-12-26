fd5b40ba138912dd94b6bf545fb915efc3d91db9
sanyl <homolkadam@seznam.cz>
2023-12-26 20:10:48 +0100

    UI updated

--- src/views/PrehledView.vue
+++ src/views/PrehledView.vue
@@ -3,30 +3,24 @@
         <div class="alert alert-danger" role="alert" v-if="!userStore.isLoggedIn">
             ❗ Pro zobrazení přehledu se musíš přihlásit!
         </div>
-        <div v-else>
-            <div class="row">
-                <div class="col-xl-3 col-md-6 my-2" v-for="item in 4" :key="item">
-                    <div class="card shadow h-100">
-                        <div class="card-body">
-                            <div class="row align-items-center">
-                                <div class="col mr-2">
-                                    <div class="mb-1 fs-6">
-                                        📋 Vyplněných cvičení</div>
-                                    <div class="fs-3 d-flex flex-row justify-content-between">
-                                        <div class="">126</div><i class="bi bi-caret-up-fill text-success"></i>
-                                    </div>
-                                </div>
-                            </div>
+        <div v-else class="container d-flex flex-column">
+            <div class="row my-2">
+                <div class="col-xl-3 col-md-6" v-for="item in 4" :key="item">
+                    <div class="container p-3 m-1 bg-dark rounded-1 shadow">
+                        <div class=" mb-1 fs-6">
+                            📋 Vyplněných cvičení</div>
+                        <div class="fs-3 d-flex flex-row justify-content-between">
+                            <div class="">126</div><i class="bi bi-caret-up-fill text-success"></i>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="container-fluid rounded-3 bg-dark p-2">
+            <div class="container rounded-3 bg-dark table-responsive">
                 <table class="table table-dark table-hover">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
+                            <th scope="col">ID</th>
+                            <th scope="col">PŘEDMĚT</th>
                             <th scope="col">Last</th>
                             <th scope="col">Handle</th>
                         </tr>
