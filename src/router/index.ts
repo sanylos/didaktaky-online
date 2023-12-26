d539d6e596020018605ee27ca5606c6b73506839
sanyl <homolkadam@seznam.cz>
2023-12-26 20:25:16 +0100

    History mode updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import PrehledView from '@/views/PrehledView.vue'
 import AddExerciseView from '@/views/AddExerciseView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
