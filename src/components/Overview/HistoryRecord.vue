fd13340dd6ccc925d4653a29e8328b160f556ca1
sanyl <homolkadam@seznam.cz>
2023-12-29 21:48:27 +0100

    Updated possible null variables

--- src/components/Overview/HistoryRecord.vue
+++ src/components/Overview/HistoryRecord.vue
@@ -1,23 +1,23 @@
 <template>
     <td>
-        {{ answer.id }}
+        {{ answer?.id }}
     </td>
     <td>
-        <span v-if="answer.examType == 'PZ'">PŘIJÍMACÍ</span>
-        <span v-if="answer.examType == 'MZ'">MATURITNÍ</span>
+        <span v-if="answer?.examType == 'PZ'">PŘIJÍMACÍ</span>
+        <span v-if="answer?.examType == 'MZ'">MATURITNÍ</span>
     </td>
     <td>
-        {{ answer.exerciseType }}
+        {{ answer?.exerciseType }}
     </td>
     <td>
-        {{ answer.examSubject }}
+        {{ answer?.examSubject }}
     </td>
     <td>
-        <span v-if="answer.isCorrect" class="fw-bold text-success">SPRÁVNĚ</span>
+        <span v-if="answer?.isCorrect" class="fw-bold text-success">SPRÁVNĚ</span>
         <spam v-else class="fw-bold text-danger">ŠPATNĚ</spam>
     </td>
     <td>
-        <span v-if="answer.answered_at" class="text-success">{{ getTimeRangeOfDate(answer.generated_at, answer.answered_at) }}s</span>
+        <span v-if="answer?.answered_at" class="text-success">{{ getTimeRangeOfDate(answer?.generated_at, answer?.answered_at) }}s</span>
         <i v-else style="font-size:12px" class="text-danger"> NEZODPOVĚZENO</i>
     </td>
 </template>
@@ -30,7 +30,7 @@ defineProps({
 const getTimeRangeOfDate = (from: string, to: string) => {
     const startDate = new Date(from);
     const endDate = new Date(to);
-    return ((endDate.getTime() - startDate.getTime()) / 1000)
+    return ((endDate.getTime() - startDate.getTime()) / 1000).toString();
 }
 </script>
 
