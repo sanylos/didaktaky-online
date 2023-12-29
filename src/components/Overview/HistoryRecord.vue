a13b514e054245a0d97ba7ef33a5b59fd610ff15
sanyl <homolkadam@seznam.cz>
2023-12-29 19:22:27 +0100

    Created HistoryRecord component

---
+++ src/components/Overview/HistoryRecord.vue
@@ -0,0 +1,28 @@
+<template>
+    <td>
+        {{ answer.id }}
+    </td>
+    <td>
+        <span v-if="answer.examType == 'PZ'">PŘIJÍMACÍ</span>
+        <span v-if="answer.examType == 'MZ'">MATURITNÍ</span>
+    </td>
+    <td>
+        {{ answer.exerciseType }}
+    </td>
+    <td>
+        {{ answer.examSubject }}
+    </td>
+    <td>
+        <span v-if="answer.isCorrect" class="fw-bold text-success">SPRÁVNĚ</span>
+        <spam v-else class="fw-bold text-danger">ŠPATNĚ</spam>
+        <i v-if="!answer.answered_at" style="font-size:12px"> (NEZODPOVĚZENO)</i>
+    </td>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    answer: Object
+})
+</script>
+
+<style></style>
