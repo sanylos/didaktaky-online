3ce8ce22f13e962b1f2afda15107e5a14f892d8f
sanyl <homolkadam@seznam.cz>
2024-01-12 19:57:46 +0100

    Added alert template

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,11 +1,17 @@
 <template>
-  
+    <div v-if="message" class="alert alert-dismissible fade show" :class="'alert-'+type" role="alert">
+        <i class="bi bi-info-circle-fill"></i>
+        <span class="ms-2">{{ message }}</span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
 </template>
 
 <script lang="ts" setup>
 
+const props = defineProps({
+    message: String,
+    type: String
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
