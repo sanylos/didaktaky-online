8809129737e859361392849189f394d985b90a46
sanyl <homolkadam@seznam.cz>
2024-01-12 20:05:52 +0100

    Added separate icons

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,6 +1,9 @@
 <template>
     <div v-if="message" class="alert alert-dismissible fade show" :class="'alert-'+type" role="alert">
-        <i class="bi bi-info-circle-fill"></i>
+        <i v-if="type=='info'" class="bi bi-info-circle-fill"></i>
+        <i v-if="type=='danger'" class="bi bi-exclamation-circle-fill"></i>
+        <i v-if="type=='success'" class="bi bi-check-circle-fill"></i>
+        <i v-if="type=='warning'" class="bi bi-exclamation-triangle-fill"></i>
         <span class="ms-2">{{ message }}</span>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
