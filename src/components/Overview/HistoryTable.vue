ca307a3c1b1ad918e38c8d8331955cd6e1250604
sanyl <homolkadam@seznam.cz>
2024-01-14 20:00:31 +0100

    Added table filtering

--- src/components/Overview/HistoryTable.vue
+++ src/components/Overview/HistoryTable.vue
@@ -1,64 +1,100 @@
 <template>
-    <div class="mx-1 my-3 fs-6">
-        <i class="bi bi-clock-history"></i><span> Historie</span>
+    <div class="mx-1 my-3 fs-6 d-flex justify-content-between">
+        <div><i class="bi bi-clock-history"></i> Historie</div>
+        <div class="d-flex">
+            <input type="checkbox" value="PZ" v-model="filter.examType" class="btn-check" id="option10" autocomplete="off"
+                checked>
+            <label class="btn btn-sm btn-outline-light me-1" for="option10">PZ</label>
+
+            <input type="checkbox" value="MZ" v-model="filter.examType" class="btn-check" id="option20" autocomplete="off">
+            <label class="btn btn-sm btn-outline-light me-1" for="option20">MZ</label>
+
+            <div class="me-1">|</div>
+
+            <input type="checkbox" value="CJL" v-model="filter.examSubject" class="btn-check" id="option6"
+                autocomplete="off" checked>
+            <label class="btn btn-sm btn-outline-light me-1" for="option6">CJL</label>
+
+            <input type="checkbox" value="MAT" v-model="filter.examSubject" class="btn-check" id="option7"
+                autocomplete="off">
+            <label class="btn btn-sm btn-outline-light me-1" for="option7">MAT</label>
+
+            <input type="checkbox" value="ANJ" v-model="filter.examSubject" class="btn-check" id="option8"
+                autocomplete="off">
+            <label class="btn btn-sm btn-outline-light" for="option8">ANJ</label>
+        </div>
     </div>
-    <div class="table-responsive">
-        <table class="table table-dark table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">TYP ZKOUŠKY</th>
-                    <th scope="col">TYP CVIČENÍ</th>
-                    <th score="col">SKUPINA CVIČENÍ</th>
-                    <th score="col">PŘEDMĚT</th>
-                    <th scope="col">ZODPOVĚZENO</th>
-                    <th scope="col">ČAS</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="answer, index in answers" :key="index">
-                    <td>
-                        {{ answer?.id }}
-                    </td>
-                    <td>
-                        <span v-if="answer?.examType == 'PZ'">PŘIJÍMACÍ</span>
-                        <span v-if="answer?.examType == 'MZ'">MATURITNÍ</span>
-                    </td>
-                    <td>
-                        {{ answer?.exerciseType }}
-                    </td>
-                    <td>
-                        {{ answer?.exerciseGroup }}
-                    </td>
-                    <td>
-                        {{ answer?.examSubject }}
-                    </td>
-                    <td>
-                        <span v-if="answer?.isCorrect" class="fw-bold text-success">SPRÁVNĚ</span>
-                        <span v-else class="fw-bold text-danger">ŠPATNĚ</span>
-                    </td>
-                    <td>
-                        <span v-if="answer?.answered_at" class="text-success">{{ getTimeRangeOfDate(answer?.generated_at,
-                            answer?.answered_at) }}s</span>
-                        <i v-else style="font-size:12px" class="text-danger">NEZODPOVĚZENO</i>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div v-if="filteredAnswers.length">
+        <div class="table-responsive">
+            <table class="table table-dark table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">TYP ZKOUŠKY</th>
+                        <th scope="col">TYP CVIČENÍ</th>
+                        <th score="col">SKUPINA CVIČENÍ</th>
+                        <th score="col">PŘEDMĚT</th>
+                        <th scope="col">ZODPOVĚZENO</th>
+                        <th scope="col">ČAS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="answer, index in filteredAnswers" :key="index">
+                        <td>
+                            {{ answer?.id }}
+                        </td>
+                        <td>
+                            <span v-if="answer?.examType == 'PZ'">PŘIJÍMACÍ</span>
+                            <span v-if="answer?.examType == 'MZ'">MATURITNÍ</span>
+                        </td>
+                        <td>
+                            {{ answer?.exerciseType }}
+                        </td>
+                        <td>
+                            {{ answer?.exerciseGroup }}
+                        </td>
+                        <td>
+                            {{ answer?.examSubject }}
+                        </td>
+                        <td>
+                            <span v-if="answer?.isCorrect" class="fw-bold text-success">SPRÁVNĚ</span>
+                            <span v-else class="fw-bold text-danger">ŠPATNĚ</span>
+                        </td>
+                        <td>
+                            <span v-if="answer?.answered_at" class="text-success">{{
+                                getTimeRangeOfDate(answer?.generated_at,
+                                    answer?.answered_at) }}s</span>
+                            <i v-else style="font-size:12px" class="text-danger">NEZODPOVĚZENO</i>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="d-flex justify-content-center mb-3 fs-6">
+            <a href="javascript:;" @click="fetchMoreData">Zobrazit více</a>
+        </div>
     </div>
-    <div class="d-flex justify-content-center mb-3 fs-6">
-        <a href="javascript:;" @click="fetchMoreData">Zobrazit více</a>
-    </div>
+    <div v-else class="text-secondary text-center fst-italic p-3 mb-3">Nebyla nalezena žádná data odpovídající
+        zadaným filtrům</div>
 </template>
 
 <script setup lang="ts">
 import { supabase } from '@/supabase';
 import { useUserStore } from '@/stores/user';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, reactive, ref } from 'vue';
+import { Obj } from '@popperjs/core';
 
 const userStore = useUserStore();
 const answers: any = ref([]);
 const pagination = ref(0);
+const filter = reactive({
+    examType: ['PZ', 'MZ'],
+    examSubject: ['CJL', 'MAT', 'ANJ']
+})
+
+const filteredAnswers = computed(() => {
+    return answers.value.filter((item: any) => filter.examSubject.includes(item.examSubject) && filter.examType.includes(item.examType))
+})
 
 const fetchMoreData = async () => {
     const { data, error: dataError } = await supabase
@@ -71,6 +107,7 @@ const fetchMoreData = async () => {
     else {
         answers.value.push(...data);
         pagination.value += 10;
+        console.log(answers.value)
     }
 }
 
