7ff2b770918bd6eb6256e634b0e142bf5773aad7
sanyl <homolkadam@seznam.cz>
2024-01-15 19:33:32 +0100

    Added spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="props.isLoading" class="d-flex justify-content-center p-5 text-white">
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    isLoading: Boolean
+})
+</script>
+
+<style></style>
