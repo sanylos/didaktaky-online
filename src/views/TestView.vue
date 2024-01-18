a4abce4f9d95de48ed3aa9d06231c670d5a6d8a0
sanyl <homolkadam@seznam.cz>
2024-01-18 20:27:20 +0100

    Added exercise counting method

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,5 +1,6 @@
 <template>
-    <div v-if="!isTest" class="container rounded d-flex flex-column justify-content-center align-items-center mt-2 bg-dark p-3">
+    <div v-if="!isTest"
+        class="container rounded d-flex flex-column justify-content-center align-items-center mt-2 bg-dark p-3">
 
         <Alert type="danger" :message="errorMessage"></Alert>
 
@@ -7,7 +8,7 @@
         <div class="d-flex mb-3">
             <div class="mx-1" v-for="option in examOptions.examTypes" :key="option.id">
                 <input type="radio" class="btn-check" name="examOption" :id="option.id" autocomplete="off"
-                    :value="option.id" v-model="selectedFilter.examType[0]">
+                    :value="option.id" v-model="selectedFilter.examType">
                 <label class="btn btn-outline-light" :for="option.id">{{ option.title }}</label>
             </div>
         </div>
@@ -25,7 +26,7 @@
         <div class="d-flex mb-3">
             <div class="mx-1" v-for="option in examOptions.examYears" :key="option">
                 <input type="checkbox" class="btn-check" name="examYear" :id="option" autocomplete="off" :value="option"
-                    v-model="selectedFilter.examYears">
+                    v-model="selectedFilter.examYear">
                 <label class="btn btn-outline-light" :for="option">{{ option }}</label>
             </div>
         </div>
@@ -34,7 +35,7 @@
         <div class="d-flex mb-3">
             <div class="mx-1" v-for="option in examOptions.examVariants" :key="option.id">
                 <input type="checkbox" class="btn-check" name="examVariant" :id="option.id" autocomplete="off"
-                    :value="option.id" v-model="selectedFilter.examVariants">
+                    :value="option.id" v-model="selectedFilter.examVariant">
                 <label class="btn btn-outline-light" :for="option.id">{{ option.title }}</label>
             </div>
         </div>
@@ -43,7 +44,7 @@
 
     </div>
     <div v-else>
-        the test
+        <div v-for="number in exerciseCount" class="text-white">O</div>
     </div>
 </template>
 
@@ -51,31 +52,47 @@
 import Alert from '@/components/Alert.vue';
 import { reactive, ref } from 'vue';
 import { supabase } from '@/supabase';
+import { FunctionsRelayError } from '@supabase/supabase-js';
 const errorMessage = ref('');
 const isTest = ref(false)
+const exerciseCount = ref(0);
 
-const selectedFilter: { examType: string[], examYears: string[], examVariants: string[], examSubject: string[] } = reactive({
-    examType: [],
-    examYears: ["2020","2021","2022","2023"],
-    examVariants: ["1","2","3","4","5"],
-    examSubject: [],
+const selectedFilter: { examType: string, examYear: string[], examVariant: string[], examSubject: string } = reactive({
+    examType: "",
+    examYear: [],
+    examVariant: [],
+    examSubject: "",
 });
 
 const handleSubmit = () => {
-    if(selectedFilter.examType.length < 1) {
+    if (selectedFilter.examType == "") {
         errorMessage.value = "Vyber si typ zkoušky"
-    } else if (selectedFilter.examSubject.length < 1) {
+    } else if (selectedFilter.examSubject == "") {
         errorMessage.value = "Vyber si předmět"
     } else {
         generateTest();
     }
 }
 
-const generateTest = () => {
-    
-
-
-    isTest.value = true;
+const generateTest = async () => {
+    let testId = 0;
+    exerciseCount.value = getExerciseCountBySubject(selectedFilter.examSubject);
+    console.log(exerciseCount);
+    const { data, error } = await supabase
+        .from('tests')
+        .select('id')
+        .eq('year', selectedFilter.examYear)
+        .eq('type', selectedFilter.examType)
+        .eq('subject', selectedFilter.examSubject)
+        .eq('variant', selectedFilter.examVariant)
+    if (data) {
+        console.log(data);
+        testId = data[0].id;
+        isTest.value = true;
+    }
+    if (error) {
+        console.log(error);
+    }
 }
 
 const examOptions = reactive({
@@ -84,7 +101,7 @@ const examOptions = reactive({
         { "id": "MZ", "title": "Maturita" },
     ],
     examSubjects: [
-        { "id": "CJL", "title": "Český jazyk a literatura", "exerciseCountPZ": 30 },
+        { "id": "CJL", "title": "Český jazyk a literatura", "exerciseCountPZ": 30, "exerciseCountMZ": 32 },
         { "id": "MAT", "title": "Matematika" },
         { "id": "ANJ", "title": "Anglický jazyk" },
     ],
@@ -97,6 +114,13 @@ const examOptions = reactive({
         { "id": "5", "title": "Ilustrační" },
     ],
 })
+
+const getExerciseCountBySubject = (subject: string) => {
+    let filteredSubject = examOptions.examSubjects.filter((item) => item.id === subject);
+    if(selectedFilter.examType == "PZ") return filteredSubject[0].exerciseCountPZ;
+    if(selectedFilter.examType == "MZ") return filteredSubject[0].exerciseCountMZ;
+    return 0;
+}
 </script>
 
 <style></style>
