60e74fa43fb9047a24e0a82f3648f89641c432d7
sanyl <homolkadam@seznam.cz>
2024-01-27 19:16:36 +0100

    Updated auth init

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -21,9 +21,9 @@ export const useUserStore = defineStore({
   actions: {
     resetUserFilters() {
       this.exerciseFilters.examType = ["PZ"];
-      this.exerciseFilters.examYears = ["2020","2021","2022","2023"];
-      this.exerciseFilters.examVariants = ["1","2","3","4","5","6"];
-      this.exerciseFilters.examSubjects = ["CJL","MAT","ANJ"];
+      this.exerciseFilters.examYears = ["2020", "2021", "2022", "2023"];
+      this.exerciseFilters.examVariants = ["1", "2", "3", "4", "5", "6"];
+      this.exerciseFilters.examSubjects = ["CJL", "MAT", "ANJ"];
     },
     resetUserData() {
       this.isLoggedIn = false;
@@ -31,13 +31,20 @@ export const useUserStore = defineStore({
       this.email = null;
     },
     async initialize() {
-      const { data: { user } } = await supabase.auth.getUser();
-      if (user) {
-        this.email = user.email;
-        this.isLoggedIn = true;
-        this.id = user.id;
-      } else {
-        this.resetUserData();
+      try {
+        const { data: { user }, error } = await supabase.auth.getUser();
+        if (user) {
+          this.email = user.email;
+          this.isLoggedIn = true;
+          this.id = user.id;
+        } else {
+          this.resetUserData();
+        }
+        if (error) {
+          console.log("Chyba autentizace");
+        }
+      } catch (error) {
+        console.log(error);
       }
     },
     async signOut() {
