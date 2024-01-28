c671293a2b4d09a751f1ad522a300121ec549ddc
sanyl <homolkadam@seznam.cz>
2024-01-28 20:36:26 +0100

    Added animated background

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="text-white">
-    <div class="rainbow_text_animated text-center fs-1">DIDAKTAKY-ONLINE</div>
-    <div class="text-center fs-5">Moderní a efektivní příprava na přijímací zkoušky a maturitu</div>
+  <div class="d-flex flex-row main">
+    <div class="rainbow_text_animated text-center fs-1">
+      <div class="d-flex flex-column">
+        <span class="fw-bold">Moderní a efektivní příprava</span>
+        <span>na přijímací zkoušky a maturitu</span>
+      </div>
+    </div>
+    <div class="text-white">cs</div>
   </div>
 </template>
 
@@ -14,14 +19,25 @@ import router from "@/router";
 </script>
 
 <style>
-.rainbow_text_animated {
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+
+.main {
+  background: linear-gradient(to right, #6e17fa 0%, #8c39ff 33%, #6C33A3 66%, #812390 100%);
+  width: 100vw;
+  height: 100vh;
+  animation: rainbow_animation 6s ease-in-out infinite;
+  background-size: 400% 100%;
+}
+
+/*.rainbow_text_animated {
+  font-family: 'Roboto';
   background: linear-gradient(to right, #6e17fa 0%, #8c39ff 33%, #6C33A3 66%, #812390 100%);
   -webkit-background-clip: text;
   background-clip: text;
   color: transparent;
   animation: rainbow_animation 6s ease-in-out infinite;
   background-size: 400% 100%;
-}
+}*/
 
 @keyframes rainbow_animation {
 
@@ -30,7 +46,7 @@ import router from "@/router";
     background-position: 0 0;
   }
 
-  25% {
+  50% {
     background-position: 100% 0;
   }
 }
